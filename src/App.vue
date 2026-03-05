201f67c0e71e055a5b8b0cdfe782a815db444a6a
mathiCV <escorpiowild@gmail.com>
2026-03-05 13:40:15 -0500

    Problem with styles

--- src/App.vue
+++ src/App.vue
@@ -9,10 +9,41 @@
 </script>
 
 <template>
-  <h1>Rick and Morty</h1>
-  <ListCharacters />
+  <div class="container">
+    <h1>Rick and Morty Characters</h1>
+    <ListCharacters/>
+  </div>
 </template>
 
-<style scoped>
+<style lang="scss">
+  :root{
+    --background-body: #24282F;
+    --background-card: #3c3e44;
+    --text-white: #FFFFFFF;
+    --text-gray: #c0c0c0;
+    --text-orange: #FF9800;
+  }
+  *{
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+  body{
+    background-color: var(--background-body);
+    color: var(--text-white);
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  }
+  h1{
+    margin-bottom: 3rem;
+    text-align: center;
+  }
+  .container{
+    width: 900px;
+    max-width: 90%;
+    margin: 5rem auto;
+  }
+  img {
+    width: 100%;
+  }
 
 </style>
