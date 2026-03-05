05d2f9918b0fbefa8da6d5b5920c25174aea571f
mathiCV <escorpiowild@gmail.com>
2026-03-04 21:15:52 -0500

    First commit: Rick and Morty

---
+++ README.md
@@ -0,0 +1,1 @@
+#Proyecto sobre VUe y su uso para un repositorio de Rick y Morty
---
+++ src/main.js
@@ -0,0 +1,29 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
+
+export default createStore({
+    state:{
+        characters: [],
+        charactersFilter: []
+
+    },
+    mutations:{
+
+    },
+    actions: {
+
+
+    },
+    
+    modules:{
+
+    }
+
+})
+
+
+
